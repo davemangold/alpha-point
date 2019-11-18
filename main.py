d8d5181033a75cb7dbddbd7f8444bb3235a5ec16
Dave Mangold <dave@davemangold.com>
2019-11-17 20:39:15 -0800

    Argument help
    Update debug argument help text

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from game import Game
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--debug', help='run game in debug mode', action='store_true')
+parser.add_argument('-d', '--debug', help='run the game in debug mode', action='store_true')
 args = parser.parse_args()
 
 
